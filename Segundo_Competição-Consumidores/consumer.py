11c0d6373ed90f8c23c5d3dcd81e4f166a291b31
gabrielhenrique-123 <gabrielherniquerocha@gmail.com>
2024-08-03 12:26:53 -0300

    Competição de Consumidores

--- Segundo_Competição-Consumidores/consumer.py
+++ Segundo_Competição-Consumidores/consumer.py
@@ -3,11 +3,11 @@ import time
 import random
 
 def on_message_received(ch, method, properties, body):
-    processing_time = random.randint(1,6)
-    print(f"Nova mensagem recebida: {body}, irá levar {processing_time} segundos para processar")
-    time.sleep(processing_time)
-    ch.basic_ack(delivery_tag = method.delivery_tag)
-    print("Mensagem processada")
+  processing_time = random.randint(1,6)
+  print(f"Nova mensagem recebida: {body}, irá levar {processing_time} segundos para processar")
+  time.sleep(processing_time)
+  ch.basic_ack(delivery_tag = method.delivery_tag)
+  print("Mensagem processada")
 
 connection_parameters = pika.ConnectionParameters('localhost')
 
@@ -17,7 +17,7 @@ channel = connection.channel()
 
 channel.queue_declare(queue = 'letterbox')  
 
-#channel.basic_qos(prefetch_count=1)
+channel.basic_qos(prefetch_count=1)
 
 channel.basic_consume(queue = 'letterbox', on_message_callback=on_message_received)
 
