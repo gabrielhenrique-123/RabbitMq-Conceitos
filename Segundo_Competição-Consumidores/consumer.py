63c17e5d3b90a2031926cffb0933c4faa0e7d8a0
gabrielhenrique-123 <gabrielherniquerocha@gmail.com>
2024-08-03 12:24:30 -0300

    Competição de Consumidores

---
+++ Segundo_Competição-Consumidores/consumer.py
@@ -0,0 +1,26 @@
+import pika
+import time
+import random
+
+def on_message_received(ch, method, properties, body):
+    processing_time = random.randint(1,6)
+    print(f"Nova mensagem recebida: {body}, irá levar {processing_time} segundos para processar")
+    time.sleep(processing_time)
+    ch.basic_ack(delivery_tag = method.delivery_tag)
+    print("Mensagem processada")
+
+connection_parameters = pika.ConnectionParameters('localhost')
+
+connection = pika.BlockingConnection(connection_parameters)
+
+channel = connection.channel()
+
+channel.queue_declare(queue = 'letterbox')  
+
+#channel.basic_qos(prefetch_count=1)
+
+channel.basic_consume(queue = 'letterbox', on_message_callback=on_message_received)
+
+print("Consumindo")
+
+channel.start_consuming()
---
+++ Segundo_Competição-Consumidores/producer.py
@@ -0,0 +1,24 @@
+import pika
+import time
+import random
+
+connection_parameters = pika.ConnectionParameters('localhost')
+
+connection = pika.BlockingConnection(connection_parameters)
+
+channel = connection.channel()
+
+channel.queue_declare(queue = 'letterbox')  
+
+messageId = 1
+
+while(True):
+  message = f"Enviando messageId: {messageId}"
+
+  channel.basic_publish(exchange = '', routing_key='letterbox', body=message)
+
+  print(f"Mensagem enviada: {message}")
+
+  time.sleep(random.randint(1,4))
+  
+  messageId+=1
